436424d6ed81727aec8879c38a1cb7db2ce7dbca
Ruyllex <ruymori630@gmail.com>
2022-10-05 16:12:47 -0300

    lee un mensaje y lo responde

---
+++ bot_windows.py
@@ -0,0 +1,45 @@
+import pywhatkit
+import pyautogui as pt
+import  pyperclip as pc
+from pynput.mouse import Button, Controller
+from time import sleep
+import webbrowser
+
+webbrowser.open("https://web.whatsapp.com")
+sleep(10)
+mouse = Controller()
+pt.FAILSAFE = True
+def nav_to_image(image, clicks , off_x = 0, off_y = 0):
+    position = pt.locateCenterOnScreen(image,confidence=0.8)
+    if position is None:
+        print(f'{image} not found xd')
+        return 0
+    else:
+        pt.moveTo(position,duration=.2)
+        pt.moveRel(off_x,off_y, duration=.2)
+        pt.click(clicks=clicks,interval=.1)
+
+def get_message():
+    nav_to_image('C://Users//ruymo//programacion//xd//bot_whatsapp//imagenes//clip_wpss.png',0,off_x=50,off_y=-80)
+    mouse.click(Button.left,3)
+    pt.rightClick()
+    sleep(1)
+    pt.moveRel(20,-320)
+    mouse.click(Button.left,2)
+    sleep(.5)
+    return pc.paste()
+    
+def check_if_new_message():
+    check = nav_to_image('C://Users//ruymo//programacion//xd//bot_whatsapp//imagenes//no_leido.png',1,off_x=-20,off_y=0)
+
+    if check == 0:
+        return False
+    else:
+        return True
+
+if check_if_new_message():
+    mensaje = get_message()
+    pt.typewrite("Hola ryu este es el nuevo bot de ruy")
+    pt.typewrite("El ultimo mensaje que mandaste fue:" + mensaje)
+    pt.press("enter")
+
