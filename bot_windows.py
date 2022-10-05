feb4c258c9a9a7aae77342e0c964fefe4c7812d8
Ruyllex <ruymori630@gmail.com>
2022-10-05 16:50:15 -0300

    ajustes

--- bot_windows.py
+++ bot_windows.py
@@ -6,13 +6,13 @@ from time import sleep
 import webbrowser
 
 webbrowser.open("https://web.whatsapp.com")
-sleep(10)
+sleep(7)
 mouse = Controller()
 pt.FAILSAFE = True
 def nav_to_image(image, clicks , off_x = 0, off_y = 0):
     position = pt.locateCenterOnScreen(image,confidence=0.8)
     if position is None:
-        print(f'{image} not found xd')
+        print(f'{image} no encontro la imagen xd lol')
         return 0
     else:
         pt.moveTo(position,duration=.2)
@@ -38,8 +38,12 @@ def check_if_new_message():
         return True
 
 if check_if_new_message():
-    mensaje = get_message()
-    pt.typewrite("Hola este es el nuevo bot de ruy")
-    pt.typewrite("El ultimo mensaje que mandaste fue:" + mensaje)
-    pt.press("enter")
+    mensaje = get_message()    
+    print(mensaje)
+    cocina_txt = open("cocina.txt","w")
+    cocina_txt.write(mensaje)
+    cocina_txt.close()
+
+
+    
 
